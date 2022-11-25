c7592246a3964ea9fd83d098e5a0c47c29bc8e6e
Spolpinka <a421243266@gmail.com>
2022-11-25 08:39:16 +0300

    Initial commit

---
+++ src/main/java/pro/sky/testjunit/demojunit/DemoJUnitApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testjunit.demojunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJUnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/testjunit/demojunit/DemoJUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.testjunit.demojunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
